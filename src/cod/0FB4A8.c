5d5e2d5db23d98e50aff9fb0ca2dd76be4b05e5c
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 18:10:06 +0000

    Match func_001FB4A8 + func_001B0A38 (multi-store sdata init)

---
+++ src/cod/0FB4A8.c
@@ -0,0 +1,16 @@
+extern int D_006337F0;
+extern int D_006337F4;
+extern int D_006337F8;
+extern int D_006337FC;
+extern int D_00633800;
+
+void func_001FB4A8(void) {
+    D_006337F0 = 0;
+    D_006337F4 = 0x80;
+    __asm__ __volatile__("" : : : "memory");
+    D_00633800 = 0x80;
+    __asm__ __volatile__("" : : : "memory");
+    D_006337FC = 0x80;
+    __asm__ __volatile__("" : : : "memory");
+    D_006337F8 = 0x80;
+}
