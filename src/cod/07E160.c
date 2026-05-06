61100cea73b78ca8dedaa1aa8c21e864bef56276
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 18:59:57 +0000

    Match func_0017E160 (set 6 sub fields)

---
+++ src/cod/07E160.c
@@ -0,0 +1,10 @@
+void func_0017E160(char *self) {
+    char *sub = *(char **)(self + 0x164);
+    *(int *)(sub + 0x328) = 0x7F;
+    *(int *)(sub + 0x33C) = 0;
+    *(int *)(sub + 0x110) = 0;
+    *(int *)(sub + 0x114) = 0;
+    *(int *)(sub + 0x118) = 0;
+    __asm__ __volatile__("" : : : "memory");
+    *(int *)(sub + 0x32C) = 0x7F;
+}
