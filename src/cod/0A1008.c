03348d6a5c1197ffb90ea64bf869a5901495bcf7
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 18:04:25 +0000

    Match func_001A1008 (init 4 sdata vars)

---
+++ src/cod/0A1008.c
@@ -0,0 +1,11 @@
+extern int D_00632888;
+extern int D_00633E30;
+extern int D_00633E24;
+extern int D_00633E44;
+
+void func_001A1008(void) {
+    D_00632888 = 0;
+    D_00633E30 = -1;
+    D_00633E24 = 0;
+    D_00633E44 = 0;
+}
