2df7f40b72ddca1de294005c2f96656cc57a857c
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 23:10:18 +0000

    Permuter: promote improved seeds for func_00105290 + func_00106AA8

--- tough_nuts/func_00105290/func_00105290.c
+++ tough_nuts/func_00105290/func_00105290.c
@@ -7,10 +7,13 @@ typedef struct
 extern CodSlot64 D_0065FA00[];
 CodSlot64 *func_00105290(void)
 {
+  CodSlot64 *new_var3;
   CodSlot64 *new_var;
   int new_var2;
   new_var2 = (new_var2 = D_00631B40);
   new_var2 = 0;
-  new_var = &D_0065FA00[new_var2];
-  return new_var2, new_var;
+ do { } while (0);
+  ;
+  new_var3 = (new_var2, &D_0065FA00[new_var2]);
+  return new_var3;
 }
--- tough_nuts/func_00106AA8/func_00106AA8.c
+++ tough_nuts/func_00106AA8/func_00106AA8.c
@@ -4,8 +4,15 @@ void func_00106AA8(void *dst, char *self)
 {
   char *new_var2;
   char *new_var;
-  new_var2 = (*((char **) new_var)) + 0x590;
+  long new_var3;
+  char *new_var4;
+  new_var2 = (new_var4 = (*((char **) new_var)) + 0x590);
+  new_var3 = 0x15C;
   new_var = dst;
-  func_00105F00(new_var, (*((char **) new_var)) + 0x590);
-  new_var = self + 0x15C;
+  new_var2 = *((char **) new_var);
+  if (!dst)
+  {
+    func_00105F00(new_var, new_var2 + 0x590);
+  }
+  new_var = self + new_var3;
 }
