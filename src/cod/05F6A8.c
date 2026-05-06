b6c38346120f39bd5b2ea0f91e0b6515c51fd39c
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 11:34:46 +0000

    Match func_0015F6A8, func_001652A0 (== K compares)

---
+++ src/cod/05F6A8.c
@@ -0,0 +1,4 @@
+int func_0015F6A8(char *self) {
+    char *sub = *(char **)(self + 0x164);
+    return *(int *)(sub + 0x30) == 0xF;
+}
---
+++ src/cod/0652A0.c
@@ -0,0 +1,4 @@
+int func_001652A0(char *self) {
+    char *sub = *(char **)(self + 0x164);
+    return *(int *)(sub + 0x430) == 2;
+}
