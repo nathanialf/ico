a7a67874c45bdb2ab27df2938c9d049ead5d75de
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 18:52:52 +0000

    Match func_001EB9F0 (zero 8 sdata vars)

---
+++ src/cod/0EB9F0.c
@@ -0,0 +1,24 @@
+extern int D_006335F8;
+extern int D_006335FC;
+extern int D_00633600;
+extern int D_00633604;
+extern int D_00633608;
+extern int D_0063360C;
+extern int D_00633610;
+extern int D_00633614;
+
+void func_001EB9F0(void) {
+    D_006335FC = 0;
+    D_00633604 = 0;
+    __asm__ __volatile__("" : : : "memory");
+    D_00633600 = 0;
+    __asm__ __volatile__("" : : : "memory");
+    D_0063360C = 0;
+    __asm__ __volatile__("" : : : "memory");
+    D_006335F8 = 0;
+    __asm__ __volatile__("" : : : "memory");
+    D_00633608 = 0;
+    __asm__ __volatile__("" : : : "memory");
+    D_00633610 = 0;
+    D_00633614 = 0;
+}
