8fd051b8a6022c821deb6a9250cb3a28b4610b20
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 18:38:11 +0000

    Match func_001CE978 (arr[idx] = 0)

---
+++ src/cod/0CE978.c
@@ -0,0 +1,6 @@
+void func_001CE978(char *self, int idx) {
+    char *sub = *(char **)(self + 0x15C);
+    char *p = *(char **)(sub + 0x800);
+    int *arr = *(int **)(p + 0x14);
+    arr[idx] = 0;
+}
