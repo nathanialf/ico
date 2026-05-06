abc0e3a4935424948b6ef8aed3957b67c8adc331
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 10:03:56 +0000

    Match 3 more 3-insn funcs (154790, 182C08, 1878F8)

---
+++ src/cod/054790.c
@@ -0,0 +1,5 @@
+extern unsigned char D_006AAB30[];
+
+unsigned char func_00154790(void) {
+    return D_006AAB30[0];
+}
---
+++ src/cod/082C08.c
@@ -0,0 +1,5 @@
+extern int D_0028A898[];
+
+void func_00182C08(void) {
+    D_0028A898[0] = 0;
+}
