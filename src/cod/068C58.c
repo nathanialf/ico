805974114f33770d28eb5d68814c9534058f32e7
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 20:41:07 +0000

    Match func_00168C58 (init 8 sdata + raw addr load)

---
+++ src/cod/068C58.c
@@ -0,0 +1,24 @@
+extern int D_00633D08;
+extern int D_00633D0C;
+extern int D_00633D10;
+extern int D_00633D14;
+extern int D_00633D18;
+extern int D_00633D1C;
+extern int D_00633D20;
+extern int D_00633D24;
+extern int D_00633D28;
+
+void func_00168C58(void) {
+    int tmp;
+    D_00633D08 = 0;
+    tmp = *(volatile int *)0x10000000;
+    D_00633D0C = 0;
+    D_00633D18 = tmp;
+    __asm__ __volatile__("" : : : "memory");
+    D_00633D10 = 0;
+    D_00633D14 = 0;
+    D_00633D1C = 0;
+    D_00633D20 = 0;
+    D_00633D24 = 0;
+    D_00633D28 = 0;
+}
