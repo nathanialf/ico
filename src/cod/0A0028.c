b999c33f18720f41bb5e4a0999637006fed25d78
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 10:10:54 +0000

    Match 3 more sdata-pair-zero patterns (1A0028, 1CBE78, 1D4D98)

---
+++ src/cod/0A0028.c
@@ -0,0 +1,7 @@
+extern int D_00632860;
+extern int D_00632864;
+
+void func_001A0028(void) {
+    D_00632860 = 0;
+    D_00632864 = 0;
+}
---
+++ src/cod/0CBE78.c
@@ -0,0 +1,7 @@
+extern int D_0063330C;
+extern int D_00633314;
+
+void func_001CBE78(void) {
+    D_0063330C = 0;
+    D_00633314 = 0;
+}
