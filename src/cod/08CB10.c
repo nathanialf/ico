0f750afb8d5b038710b7a9da2274ec6687f30708
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 10:15:17 +0000

    Match func_0018CB10, func_001E0978 (pair-arg sdata setters)

---
+++ src/cod/08CB10.c
@@ -0,0 +1,7 @@
+extern int D_00633D84;
+extern int D_00633D88;
+
+void func_0018CB10(int a, int b) {
+    D_00633D84 = a;
+    D_00633D88 = b;
+}
---
+++ src/cod/0E0978.c
@@ -0,0 +1,7 @@
+extern int D_00633438;
+extern int D_0063343C;
+
+void func_001E0978(int a, int b) {
+    D_00633438 = a;
+    D_0063343C = b;
+}
