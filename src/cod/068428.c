33134e8fe9fa59afe904d0bfef0892d2b7e018b8
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 17:08:12 +0000

    Match 6 indirect-call wrappers via D_006323F0 (a1=1..7 variants)

---
+++ src/cod/068428.c
@@ -0,0 +1,5 @@
+extern void (*D_006323F0)(int, int);
+
+void func_00168428(int arg) {
+    D_006323F0(arg, 7);
+}
---
+++ src/cod/068468.c
@@ -0,0 +1,5 @@
+extern void (*D_006323F0)(int, int);
+
+void func_00168468(int arg) {
+    D_006323F0(arg, 5);
+}
