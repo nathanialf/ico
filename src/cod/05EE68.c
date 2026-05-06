ede5e1f7650dc7aa49f679e040df9775f53e04ef
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-06 09:21:10 +0000

    Match 4 more 0x164/0x10 sub-pointer accessors -> 0.12% .text

---
+++ src/cod/05EE68.c
@@ -0,0 +1,3 @@
+void func_0015EE68(char *self, int unused, int val) {
+    *(int *)(*(char **)(self + 0x164) + 0x180) = val;
+}
---
+++ src/cod/0BB800.c
@@ -0,0 +1,3 @@
+void func_001BB800(char *self, int val) {
+    *(int *)(*(char **)(self + 0x164) + 0x98) = val;
+}
