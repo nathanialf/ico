2c6eb3dec8161c8b665599364091df16ba95c066
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-07 11:44:00 +0000

    Match func_0017B568 (call+tail-call) + func_00187280 (2 sym-arg calls)

---
+++ src/cod/087280.c
@@ -0,0 +1,9 @@
+extern int D_00632610;
+extern int D_00632614;
+extern void func_00186A10(int a0, int a1);
+
+void func_00187280(int a0)
+{
+    func_00186A10(D_00632610, a0);
+    func_00186A10(D_00632614, a0);
+}
