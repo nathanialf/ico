2808062a11d7a1e5f219fac332ddf60e88a1f13d
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-07 08:34:06 +0000

    Match func_0016A2F8 + func_0026E5F8: 2-jal pipe-result wrappers

---
+++ src/cod/16E5F8.c
@@ -0,0 +1,8 @@
+extern int func_0026E5C8(int a0, int a1);
+extern void func_002633B8(int a0);
+
+void func_0026E5F8(int a0, int a1)
+{
+    func_002633B8(func_0026E5C8(a0, a1));
+    __asm__ __volatile__("");
+}
