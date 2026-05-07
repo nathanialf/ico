68255ca2592e0e3b708206e9b5c451ab2be6647f
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-07 08:25:36 +0000

    Match 3 multi-jal wrappers: 1009E0 (3-call sequence), 1118B8/E8 (2-call + sdata-set siblings)

---
+++ src/cod/0118B8.c
@@ -0,0 +1,10 @@
+extern void func_001FBA80(void);
+extern void func_0010F5D8(void);
+extern int D_00631BA0;
+
+void func_001118B8(void)
+{
+    func_001FBA80();
+    func_0010F5D8();
+    D_00631BA0 = 1;
+}
---
+++ src/cod/0118E8.c
@@ -0,0 +1,10 @@
+extern void func_001FBA80(void);
+extern void func_0010F710(void);
+extern int D_00631BA0;
+
+void func_001118E8(void)
+{
+    func_001FBA80();
+    func_0010F710();
+    D_00631BA0 = 1;
+}
