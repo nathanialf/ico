c059ab5e8ff21e12aec96665074aa3a52803e45a
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-07 21:05:43 +0000

    Match func_00154770: sentinel-byte gate + return field
    Direct C; main subtlety was that ee-gcc 2.9 needs the if-true branch
    to express the active path (return *(int*)(arr+4)) and the if-false
    branch to be the fall-through return 0, otherwise it inverts the
    sense of the beq into bne and swaps the leaf order.

---
+++ src/cod/054770.c
@@ -0,0 +1,9 @@
+extern unsigned char D_006AAB30[];
+
+int func_00154770(void)
+{
+    if (D_006AAB30[0]) {
+        return *(int *)(D_006AAB30 + 4);
+    }
+    return 0;
+}
--- tough_nuts/func_00154770/func_00154770.c
+++
@@ -1,9 +0,0 @@
-extern unsigned char D_006AAB30;
-
-int func_00154770(void)
-{
-    if (D_006AAB30 == 0) {
-        return 0;
-    }
-    return *(int *)(((char *)&D_006AAB30) + 0x4);
-}
