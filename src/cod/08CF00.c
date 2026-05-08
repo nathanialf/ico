8e8a8aaf538e936f9b28dfdac513f39e787095a7
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-08 10:28:05 +0000

    Match 2: func_001C2EC8 (chain walker via func_0013EB50/EBE0+func_001C2760) + func_0018CF00 (3-float pair-copy guarded by D_00633D90!=3)

---
+++ src/cod/08CF00.c
@@ -0,0 +1,15 @@
+extern int D_00633D90;
+extern float D_006D0680[];
+extern float D_006D06A0[];
+
+void func_0018CF00(float *src)
+{
+    if (D_00633D90 != 3) {
+        D_006D0680[0] = src[0];
+        D_006D0680[1] = src[1];
+        D_006D0680[2] = src[2];
+        D_006D06A0[0] = src[0];
+        D_006D06A0[1] = src[1];
+        D_006D06A0[2] = src[2];
+    }
+}
