64e4de9c6b990155abade6a093ced95ef8715330
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-09 14:35:12 +0000

    Restore matched func_001AE420 (was inadvertently reverted to INCLUDE_ASM); remove stale asm/nonmatchings

--- src/cod/0AE420.c
+++ src/cod/0AE420.c
@@ -1,3 +1,9 @@
-#include "common.h"
+extern void *func_001ADED8(int a, int b);
 
-INCLUDE_ASM("asm/nonmatchings/cod/0AE420", func_001AE420);
+void *func_001AE420(int a0, int a1, int a2, int a3)
+{
+    void *r = func_001ADED8(a0, a3);
+    *(int *)((char *)r + 0x30) = a1;
+    *(int *)((char *)r + 0x34) = a2;
+    return r;
+}
