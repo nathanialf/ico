050a5467ca5c8b99bd7f396110600d37d7a5bdf3
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-12 23:15:13 +0000

    Park func_00154CE8 — beql/bnel emission diff

---
+++ tough_nuts/func_00154CE8/054CE8.c
@@ -0,0 +1,15 @@
+extern int D_006AAAE0[100];
+
+void func_00154CE8(int *a0, int *a1)
+{
+    if (a0 == 0) {
+        D_006AAAE0[0] = 0;
+    } else {
+        D_006AAAE0[0] = a0[2];
+    }
+    if (a1 == 0) {
+        D_006AAAE0[1] = 0;
+    } else {
+        D_006AAAE0[1] = a1[2];
+    }
+}
