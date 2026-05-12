5ad765e298c91e2c8ae4f26d2dea70e874c2de9e
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-12 15:48:07 +0000

    Match 4 more from permuter score-0 promotion (func_0023EDF0, 002453D0, 002456F8, 0024C038)

---
+++ src/cod/13EDF0.c
@@ -0,0 +1,22 @@
+extern void func_00241F20(int *a0);
+void func_0023EDF0(int *a0, int flag)
+{
+  int src;
+  int new_var;
+  new_var = flag;
+  if (new_var != 0)
+  {
+    src = a0[0x2C / 4];
+  }
+  else
+  {
+    src = a0[0x28 / 4];
+  }
+  {
+    int cur = a0[0x10 / 4];
+    cur = cur & (~0x1FF);
+    cur = cur | (src & 0x1FF);
+    a0[0x10 / 4] = cur;
+  }
+  return func_00241F20(a0);
+}
---
+++ src/cod/1456F8.c
@@ -0,0 +1,13 @@
+extern int D_00713000[];
+int *func_002456F8(int a0)
+{
+  int *base = D_00713000;
+  int *p = &base[4];
+  base[1] = 0;
+  base[0] = a0;
+  base[2] = (base[3] = (int) p);
+  if (1)
+  {
+    return base;
+  }
+}
