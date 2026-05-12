f007e342558c0ce3db7b638a943ee9ad0381b81f
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-12 16:11:18 +0000

    Park func_0013C4C0 — multi-field init: final sb 0x27 reordered

---
+++ tough_nuts/func_0013C4C0/03C4C0.c
@@ -0,0 +1,7 @@
+
+void func_0013C4C0(char *a0, int a1, int a2, char a3, char t0)
+{
+  short *new_var;
+ do { *((char *) (a0 + 3)) = t0; *((char *) (a0 + 0)) = 0x11; *((int *) (a0 + 4)) = a1; new_var = a0; *((int *) (new_var + 0x14)) = a2; *((char *) (a0 + 2)) = a3; } while (0); new_var = (short *) (a0 + 0x10);
+  *((char *) (a0 + 0x27)) = (*((short *) (new_var + 0x1A)) = (*((short *) (a0 + 0x1C)) = (*((short *) (new_var + 0x20)) = (*((short *) (a0 + 0x22)) = (*((short *) (a0 + 0x18)) = (*((char *) (0x26 + a0)) = (*((short *) (a0 + 0xA)) = (*((short *) (a0 + 0xC)) = (*new_var = (*((short *) (new_var + 0x12)) = (*((short *) (a0 + 8)) = (*((char *) (a0 + 0x24)) = 0))))))))))));
+}
