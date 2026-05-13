232c6db76c40bf34174085abdc02a0a20bac8261
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-13 20:06:12 +0000

    Park func_001A3340 — ld ra placement

--- src/cod/145228.c
+++ src/cod/145228.c
@@ -1,10 +1,3 @@
-void func_00245228(int **a0)
-{
-    int *p1 = a0[0];
-    int *p2 = a0[3];
-    int t = (int)p1 - 4;
-    int diff;
-    a0[3] = 0;
-    diff = t - (int)p2;
-    *p2 += (unsigned int)(diff >> 2) >> 2;
-}
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/cod/145228", func_00245228);
