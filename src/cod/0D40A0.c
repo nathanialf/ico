2bfacf653d20cf7bb9eaf77e8e001738af7c2f6a
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-13 20:41:35 +0000

    Park func_001949B8 — v0/v1 reg alloc swap

--- src/cod/0D40A0.c
+++ src/cod/0D40A0.c
@@ -1,15 +1,15 @@
 int func_001D40A0(char *a0)
 {
-    char *sub;
-    int *p;
-    int ret;
-    sub = *(char **)(a0 + 0x15C);
-    p = *(int **)(sub + 0x800);
-    ret = 0;
-    if (p[0] == 1) goto one;
-    if (*(int *)(a0 + 0x16C) != 0) goto end;
-one:
-    ret = 1;
+    int *p15c = *((int **)(a0 + 0x15C));
+    int *v0_p800 = (int *)*((int *)((char *)p15c + 0x800));
+    int v1 = (int)v0_p800;
+    int new_var2 = *((int *)(a0 + 0x16C));
+    int result;
+    if (v1 == 1) goto set;
+    result = 0;
+    if (new_var2 != 0) goto end;
+set:
+    result = 1;
 end:
-    return ret;
+    return result;
 }
