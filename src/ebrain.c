8a86d089eecaf59734123e620560afadd9269151
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-17 16:27:15 +0000

    Coalesce src/ebrain TU (7 INCLUDE_ASM + 1 matched func)
    3-way split: [0x0906B8, asm, src/cod/0906B8] +
    [0x090D70, c, src/ebrain] (0x190D70..0x191C80) +
    [0x091C80, asm, src/cod/091C80].
    
    Removed [0x0918A8, c, src/cod/0918A8] — matched body migrated.
    Stripped lit4/sdata defs from src/ebrain.c per gp_rel pattern.

--- src/ebrain.c
+++ src/ebrain.c
@@ -17,61 +17,6 @@
  * asm-generated and sidecar definitions.
  */
 
-__attribute__((section(".lit4.0x00630F9C"))) float D_00630F9C = 250000.0f;
-__attribute__((section(".lit4.0x00630FA0"))) float D_00630FA0 = 250000.0f;
-__attribute__((section(".lit4.0x00630FA4"))) float D_00630FA4 = 22500.0f;
-__attribute__((section(".lit4.0x00630FA8"))) float D_00630FA8 = 22500.0f;
-__attribute__((section(".lit4.0x00630FAC"))) float D_00630FAC = 22500.0f;
-__attribute__((section(".lit4.0x00630FB0"))) float D_00630FB0 = 3.14159274f;
-__attribute__((section(".lit4.0x00630FB4"))) float D_00630FB4 = 3.14159274f;
-__attribute__((section(".lit4.0x00630FB8"))) float D_00630FB8 = 3.14159274f;
-__attribute__((section(".lit4.0x00630FBC"))) float D_00630FBC = 3.14159274f;
-__attribute__((section(".lit4.0x00630FC0"))) float D_00630FC0 = 3.14159274f;
-__attribute__((section(".lit4.0x00630FC4"))) float D_00630FC4 = 0.1f;
-__attribute__((section(".lit4.0x00630FC8"))) float D_00630FC8 = 0.008726646f;
-__attribute__((section(".lit4.0x00630FCC"))) float D_00630FCC = 3.14159274f;
-__attribute__((section(".lit4.0x00630FD0"))) float D_00630FD0 = 3.14159274f;
-__attribute__((section(".lit4.0x00630FD4"))) float D_00630FD4 = 3.14159274f;
-__attribute__((section(".lit4.0x00630FD8"))) float D_00630FD8 = 10430.3779f;
-__attribute__((section(".lit4.0x00630FDC"))) float D_00630FDC = 0.7f;
-__attribute__((section(".lit4.0x00630FE0"))) float D_00630FE0 = 6.28318548f;
-__attribute__((section(".lit4.0x00630FE4"))) float D_00630FE4 = 0.08726647f;
-__attribute__((section(".lit4.0x00630FE8"))) float D_00630FE8 = 6.28318548f;
-__attribute__((section(".lit4.0x00630FEC"))) float D_00630FEC = 0.28f;
-__attribute__((section(".lit4.0x00630FF0"))) float D_00630FF0 = 0.28f;
-__attribute__((section(".lit4.0x00630FF4"))) float D_00630FF4 = 0.4f;
-__attribute__((section(".lit4.0x00630FF8"))) float D_00630FF8 = 0.7f;
-__attribute__((section(".lit4.0x00630FFC"))) float D_00630FFC = 6.28318548f;
-__attribute__((section(".lit4.0x00631000"))) float D_00631000 = 6.28318548f;
-__attribute__((section(".lit4.0x00631004"))) float D_00631004 = 0.2f;
-__attribute__((section(".lit4.0x00631008"))) float D_00631008 = 6.28318548f;
-__attribute__((section(".lit4.0x0063100C"))) float D_0063100C = 0.0166666675f;
-__attribute__((section(".lit4.0x00631010"))) float D_00631010 = 6.28318548f;
-__attribute__((section(".lit4.0x00631014"))) float D_00631014 = 6.28318548f;
-__attribute__((section(".lit4.0x00631018"))) float D_00631018 = 0.0349065848f;
-__attribute__((section(".lit4.0x0063101C"))) float D_0063101C = -0.0349065848f;
-__attribute__((section(".lit4.0x00631020"))) float D_00631020 = 0.6f;
-__attribute__((section(".lit4.0x00631024"))) float D_00631024 = -1.3f;
-__attribute__((section(".lit4.0x00631028"))) float D_00631028 = 0.6f;
-__attribute__((section(".lit4.0x0063102C"))) float D_0063102C = 0.142857149f;
-__attribute__((section(".lit4.0x00631030"))) float D_00631030 = 0.2f;
-__attribute__((section(".lit4.0x00631034"))) float D_00631034 = 0.001f;
-__attribute__((section(".lit4.0x00631038"))) float D_00631038 = 6.28318548f;
-__attribute__((section(".lit4.0x0063103C"))) float D_0063103C = 0.98f;
-__attribute__((section(".lit4.0x00631040"))) float D_00631040 = 0.5235988f;
-__attribute__((section(".lit4.0x00631044"))) float D_00631044 = 0.02f;
-__attribute__((section(".lit4.0x00631048"))) float D_00631048 = 1.2f;
-__attribute__((section(".lit4.0x0063104C"))) float D_0063104C = 6.28318548f;
-__attribute__((section(".lit4.0x00631050"))) float D_00631050 = 10430.3779f;
-__attribute__((section(".lit4.0x00631054"))) float D_00631054 = 1.57079637f;
-__attribute__((section(".lit4.0x00631058"))) float D_00631058 = 1.57079637f;
-__attribute__((section(".lit4.0x0063105C"))) float D_0063105C = 10430.3779f;
-__attribute__((section(".lit4.0x00631060"))) float D_00631060 = 6.28318548f;
-__attribute__((section(".lit4.0x00631064"))) float D_00631064 = 10430.3779f;
-__attribute__((section(".lit4.0x00631068"))) float D_00631068 = 0.001f;
-__attribute__((section(".sdata.0x00632774"))) int D_00632774 = 0;
-__attribute__((section(".sdata.0x00632778"))) char D_00632778[8] = { 0 };
-__attribute__((section(".sdata.0x00632780"))) int D_00632780 = 0;
 
 __attribute__((section(".rodata.0x0055AE08"))) const char D_0055AE08[16] = "src/ebrain.c";
 
@@ -87,3 +32,37 @@ __attribute__((section(".rodata.0x0055ADE0"))) const char D_0055ADE0[40] = "eBra
 __attribute__((section(".rodata.0x005F2FF8"))) const float D_005F2FF8[16] = { 5.356473e+07f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 255.0f, 255.0f, 255.0f, 255.0f, 0.0f, 128.0f, 255.0f };
 
 /* Single-float rodata migrated from ebrain_data.c */
+
+#include "include_asm.h"
+
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_00190D70);
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_00190F30);
+
+extern int D_00632778;
+extern int D_00632774;
+extern int D_00633DAC;
+extern int D_00633DB0;
+extern int D_006D0710[];
+
+void func_001918A8(void)
+{
+    int *p = D_006D0710;
+    int i;
+    D_00632778 = 0;
+    D_00632774 = 0;
+    D_00633DAC = 0;
+    p = (int *)((char *)p + 0x37C);
+    D_00633DB0 = 0;
+    i = 0x1F;
+    do {
+        *p = 0;
+        p = (int *)((char *)p - 0x1C);
+        i--;
+    } while (i >= 0);
+}
+
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_001918EC);
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_001918F0);
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_001919A0);
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_00191AA0);
+INCLUDE_ASM("asm/nonmatchings/src/ebrain", func_00191B70);
