b83fb2901755b26bbb3f9812691604f5eac48569
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-17 17:52:51 +0000

    Coalesce src/Primitive TU (13 INCLUDE_ASM funcs)
    3-way split [0x01BFC0, asm]:
      [0x01BFC0, asm, src/cod/01BFC0] — pre-Primitive (func_0011BFC0)
      [0x01C028, c, src/Primitive]    — 0x11C028..0x11DEE0 (13 funcs)

--- src/Primitive.c
+++ src/Primitive.c
@@ -17,36 +17,6 @@
  * asm-generated and sidecar definitions.
  */
 
-__attribute__((section(".lit4.0x00630A54"))) float D_00630A54 = 6.28318548f;
-__attribute__((section(".lit4.0x00630A58"))) float D_00630A58 = 10430.3779f;
-__attribute__((section(".lit4.0x00630A5C"))) float D_00630A5C = 6.28318548f;
-__attribute__((section(".lit4.0x00630A60"))) float D_00630A60 = 10430.3779f;
-__attribute__((section(".lit4.0x00630A64"))) float D_00630A64 = 4095.0f;
-__attribute__((section(".lit4.0x00630A68"))) float D_00630A68 = 3.14159274f;
-__attribute__((section(".lit4.0x00630A6C"))) float D_00630A6C = 10430.3779f;
-__attribute__((section(".lit4.0x00630A70"))) float D_00630A70 = 1.41421354f;
-__attribute__((section(".lit4.0x00630A74"))) float D_00630A74 = 1.41421354f;
-__attribute__((section(".lit4.0x00630A78"))) float D_00630A78 = 3.14159274f;
-__attribute__((section(".lit4.0x00630A7C"))) float D_00630A7C = 10430.3779f;
-__attribute__((section(".lit4.0x00630A80"))) float D_00630A80 = 3.14159274f;
-__attribute__((section(".lit4.0x00630A84"))) float D_00630A84 = 10430.3779f;
-__attribute__((section(".lit4.0x00630A88"))) float D_00630A88 = 1e-05f;
-__attribute__((section(".lit4.0x00630A8C"))) float D_00630A8C = 0.01f;
-__attribute__((section(".lit4.0x00630A90"))) float D_00630A90 = -2047.0f;
-__attribute__((section(".lit4.0x00630A94"))) float D_00630A94 = 3.14159274f;
-__attribute__((section(".lit4.0x00630A98"))) float D_00630A98 = 0.2f;
-__attribute__((section(".sdata.0x00631C54"))) int D_00631C54 = 0;
-__attribute__((section(".sdata.0x00631C58"))) int D_00631C58 = 0;
-__attribute__((section(".sdata.0x00631D28"))) const char D_00631D28[8] = "0";
-__attribute__((section(".sdata.0x00631D30"))) const char D_00631D30[8] = "%s";
-__attribute__((section(".sdata.0x00631D38"))) const char D_00631D38[8] = "FALSE";
-__attribute__((section(".sdata.0x00631D40"))) char D_00631D40[8] = { 0 };
-__attribute__((section(".sdata.0x00632CA4"))) int D_00632CA4 = 0;
-__attribute__((section(".sdata.0x00632CA8"))) int D_00632CA8 = 0;
-__attribute__((section(".sdata.0x00632CAC"))) int D_00632CAC = 0;
-__attribute__((section(".sdata.0x00632D2C"))) int D_00632D2C = 0;
-__attribute__((section(".sdata.0x00632D80"))) int D_00632D80 = 0;
-__attribute__((section(".sdata.0x00632D84"))) int D_00632D84 = 0;
 
 __attribute__((section(".rodata.0x00555490"))) const char D_00555490[16] = "src/Primitive.c";
 
@@ -65,3 +35,19 @@ __attribute__((section(".rodata.0x005554F0"))) const float D_005554F0[4] = { 307
 /* EUC-JP: "prim_DispParticle:指定したテクスチャ番号が異常です. %s:%d
 " */
 __attribute__((section(".rodata.0x00555578"))) const char D_00555578[64] = "prim_DispParticle:\273\330\304\352\244\267\244\277\245\306\245\257\245\271\245\301\245\343\310\326\271\346\244\254\260\333\276\357\244\307\244\271. %s:%d\n";
+
+#include "include_asm.h"
+
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011C028);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011C2B8);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011C500);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011C868);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011CBF8);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011CE90);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011D2E8);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011D590);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011D658);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011D730);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011D800);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011D9A0);
+INCLUDE_ASM("asm/nonmatchings/src/Primitive", func_0011DD08);
