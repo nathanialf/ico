d12da995369e4112113c05ba2873b2e9a3bfb75c
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-18 05:58:26 +0000

    Match cod/08CF60 (func_0018CF60): D_00632730 ? &D_006D0680 : NULL
    7-insn conditional pointer return + 1 trailing pad nop.

---
+++ src/cod/08CF60.c
@@ -0,0 +1,12 @@
+#include "matching.h"
+
+extern int D_00632730;
+extern int D_006D0680[];
+
+int *func_0018CF60(void)
+{
+    if (D_00632730 == 0) return 0;
+    return D_006D0680;
+}
+
+TRAILING_PAD_NOP();
