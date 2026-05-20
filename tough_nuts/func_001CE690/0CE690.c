ede5774f2cf3b630028bfee6b7f537b5b734bf20
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-20 16:05:10 +0000

    auto_permute: archive 8 broken seeds where target function isn't defined
    Caught by 'Function not found in base.c' permuter errors. Seeds where
    the .c file defines a different function (usually the next one over)
    than the parked-target name will permuter-error out immediately:
    no useful work, just wastes a worker slot.

--- tough_nuts/func_001CE690/0CE690.c
+++ tough_nuts/func_001CE690/0CE690.c
@@ -5,9 +5,11 @@ extern void func_001CE5F8(int *self);
 void func_001CE690(int *self)
 {
   int new_var;
+  unsigned long new_var2;
   long long flags = *((long long *) (((char *) ((int *) self[0x164 / 4])) + 0x18));
   new_var = 1;
-  if ((flags >> 33) & new_var)
+  new_var2 = flags;
+  if ((new_var2 >> 33) & new_var)
   {
     func_001BB7E0();
     if (func_00165F88(self) == 0)
