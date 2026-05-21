3b2d170e50ad268477a4cd54070759f57e267b51
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-21 15:51:58 +0000

    phase3d: unblock src/queen.c (45 attrs stripped)

--- src/queen.c
+++ src/queen.c
@@ -18,58 +18,95 @@
  */
 
 
-__attribute__((section(".rodata.0x0055C690"))) const char D_0055C690[16] = "src/queen.c";
+const float D_0055C2C0[12] = { 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.0f };
 
-__attribute__((section(".rodata.0x0055C620"))) const char D_0055C620[40] = "n_enemy_max:%d n_enemy:%d counter:%d";
-__attribute__((section(".rodata.0x0055C658"))) const char D_0055C658[24] = "queen_ball_scr";
-__attribute__((section(".rodata.0x0055C6C8"))) const char D_0055C6C8[16] = "damage:%d";
-__attribute__((section(".rodata.0x0055CEB0"))) const char D_0055CEB0[32] = "../ito/include/mv_defs.h";
-__attribute__((section(".rodata.0x0055CF10"))) const char D_0055CF10[32] = "../ito/include/mv_defs.h";
-__attribute__((section(".rodata.0x0055CF30"))) const char D_0055CF30[16] = "p != NULL";
-__attribute__((section(".rodata.0x0055CF70"))) const char D_0055CF70[40] = "sceMpegGetPicture() decode error";
-__attribute__((section(".rodata.0x0055CFA8"))) const char D_0055CFA8[32] = "../ito/include/mv_defs.h";
-__attribute__((section(".rodata.0x0055CFC8"))) const char D_0055CFC8[16] = "p != NULL";
+const float D_0055C2F0[66] = { 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f };
 
-/* String rodata migrated from queen_data.c */
-__attribute__((section(".rodata.0x0055C5C0"))) const char D_0055C5C0[16] = "queen attacked\n";
-__attribute__((section(".rodata.0x0055C5D0"))) const char D_0055C5D0[16] = "enemy dead %p\n";
-__attribute__((section(".rodata.0x0055C648"))) const char D_0055C648[16] = "queen dead\n";
-__attribute__((section(".rodata.0x0055C6A0"))) const char D_0055C6A0[24] = "queen barrier attacked\n";
-__attribute__((section(".rodata.0x0055C6D8"))) const char D_0055C6D8[16] = "mail %d\n";
-__attribute__((section(".rodata.0x0055C6E8"))) const char D_0055C6E8[24] = "queen ball attacked\n";
-__attribute__((section(".rodata.0x0055C720"))) const char D_0055C720[32] = "queen barrier damaged\n";
-__attribute__((section(".rodata.0x0055CD98"))) const char D_0055CD98[16] = "movie pause\n";
-__attribute__((section(".rodata.0x0055CDA8"))) const char D_0055CDA8[16] = "D_CTRL %x\n";
-__attribute__((section(".rodata.0x0055CDB8"))) const char D_0055CDB8[24] = "open movie file %s\n";
-__attribute__((section(".rodata.0x0055CDD0"))) const char D_0055CDD0[32] = "create video decode thread\n";
-__attribute__((section(".rodata.0x0055CDF0"))) const char D_0055CDF0[16] = "start thread\n";
-__attribute__((section(".rodata.0x0055CE00"))) const char D_0055CE00[16] = "add intc\n";
-__attribute__((section(".rodata.0x0055CE10"))) const char D_0055CE10[24] = "add intc failed\n";
-__attribute__((section(".rodata.0x0055CE28"))) const char D_0055CE28[16] = "add dmac\n";
-__attribute__((section(".rodata.0x0055CE38"))) const char D_0055CE38[24] = "add dmac failed\n";
-__attribute__((section(".rodata.0x0055CE50"))) const char D_0055CE50[24] = "sceGsGetIMR() %lx\n";
-__attribute__((section(".rodata.0x0055CE68"))) const char D_0055CE68[16] = "dmac %d %d\n";
-__attribute__((section(".rodata.0x0055CE78"))) const char D_0055CE78[16] = "intc %d %d\n";
-__attribute__((section(".rodata.0x0055CE88"))) const char D_0055CE88[24] = "movie init failed\n";
-__attribute__((section(".rodata.0x0055CEA0"))) const char D_0055CEA0[16] = "movie end\n";
-__attribute__((section(".rodata.0x0055CF40"))) const char D_0055CF40[24] = "pts buffer overflow\n";
-__attribute__((section(".rodata.0x0055CF58"))) const char D_0055CF58[24] = "decode thread: aborted\n";
-__attribute__((section(".rodata.0x0055CF98"))) const char D_0055CF98[16] = "movie %d x %d\n";
+const float D_0055C3F8[12] = { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
 
-/* Float rodata migrated from queen_data.c */
-__attribute__((section(".rodata.0x0055C2C0"))) const float D_0055C2C0[12] = { 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.0f };
-__attribute__((section(".rodata.0x0055C2F0"))) const float D_0055C2F0[66] = { 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f };
-__attribute__((section(".rodata.0x0055C3F8"))) const float D_0055C3F8[12] = { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
-__attribute__((section(".rodata.0x0055C428"))) const float D_0055C428[12] = { 3.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
-__attribute__((section(".rodata.0x0055C458"))) const float D_0055C458[12] = { 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 0.0f };
-__attribute__((section(".rodata.0x0055C488"))) const float D_0055C488[12] = { 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.0f };
-__attribute__((section(".rodata.0x0055C4B8"))) const float D_0055C4B8[66] = { 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f };
-__attribute__((section(".rodata.0x0055C700"))) const float D_0055C700[4] = { 0.0f, 1.0f, 0.0f, 1.0f };
-__attribute__((section(".rodata.0x0055C740"))) const float D_0055C740[4] = { 2.5e+02f, 1.5e+02f, 2e+02f, 0.0f };
-__attribute__((section(".rodata.0x0055C750"))) const float D_0055C750[4] = { 1e+02f, 1.2e+02f, 115.0f, 0.0f };
+const float D_0055C428[12] = { 3.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f };
 
-/* String-pool rodata migrated from queen_data.c */
-__attribute__((section(".rodata.0x0055CED0"))) const char D_0055CED0[64] = "p != NULL\000\000\000\000\000\000\000../ito/include/mv_defs.h\000\000\000\000\000\000\000\000p != NULL\000\000\000\000\000\000";
+const float D_0055C458[12] = { 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 4.0f, 0.0f };
+
+const float D_0055C488[12] = { 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.6f, 0.0f };
+
+const float D_0055C4B8[66] = { 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f, 0.0f, 0.0f, 0.001f, 0.01f, 0.99f, 0.99f };
+
+const char D_0055C5C0[16] = "queen attacked\n";
+
+const char D_0055C5D0[16] = "enemy dead %p\n";
+
+const char D_0055C620[40] = "n_enemy_max:%d n_enemy:%d counter:%d";
+
+const char D_0055C648[16] = "queen dead\n";
+
+const char D_0055C658[24] = "queen_ball_scr";
+
+const char D_0055C690[16] = "src/queen.c";
+
+const char D_0055C6A0[24] = "queen barrier attacked\n";
+
+const char D_0055C6C8[16] = "damage:%d";
+
+const char D_0055C6D8[16] = "mail %d\n";
+
+const char D_0055C6E8[24] = "queen ball attacked\n";
+
+const float D_0055C700[4] = { 0.0f, 1.0f, 0.0f, 1.0f };
+
+const char D_0055C720[32] = "queen barrier damaged\n";
+
+const float D_0055C740[4] = { 2.5e+02f, 1.5e+02f, 2e+02f, 0.0f };
+
+const float D_0055C750[4] = { 1e+02f, 1.2e+02f, 115.0f, 0.0f };
+
+const char D_0055CD98[16] = "movie pause\n";
+
+const char D_0055CDA8[16] = "D_CTRL %x\n";
+
+const char D_0055CDB8[24] = "open movie file %s\n";
+
+const char D_0055CDD0[32] = "create video decode thread\n";
+
+const char D_0055CDF0[16] = "start thread\n";
+
+const char D_0055CE00[16] = "add intc\n";
+
+const char D_0055CE10[24] = "add intc failed\n";
+
+const char D_0055CE28[16] = "add dmac\n";
+
+const char D_0055CE38[24] = "add dmac failed\n";
+
+const char D_0055CE50[24] = "sceGsGetIMR() %lx\n";
+
+const char D_0055CE68[16] = "dmac %d %d\n";
+
+const char D_0055CE78[16] = "intc %d %d\n";
+
+const char D_0055CE88[24] = "movie init failed\n";
+
+const char D_0055CEA0[16] = "movie end\n";
+
+const char D_0055CEB0[32] = "../ito/include/mv_defs.h";
+
+const char D_0055CED0[64] = "p != NULL\000\000\000\000\000\000\000../ito/include/mv_defs.h\000\000\000\000\000\000\000\000p != NULL\000\000\000\000\000\000";
+
+const char D_0055CF10[32] = "../ito/include/mv_defs.h";
+
+const char D_0055CF30[16] = "p != NULL";
+
+const char D_0055CF40[24] = "pts buffer overflow\n";
+
+const char D_0055CF58[24] = "decode thread: aborted\n";
+
+const char D_0055CF70[40] = "sceMpegGetPicture() decode error";
+
+const char D_0055CF98[16] = "movie %d x %d\n";
+
+const char D_0055CFA8[32] = "../ito/include/mv_defs.h";
+
+const char D_0055CFC8[16] = "p != NULL";
 
 #include "include_asm.h"
 #include "regpin.h"
