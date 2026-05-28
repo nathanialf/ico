d3d20d4e1d324e004a7bf3eb68b3daf5a5115418
Nathanial Fine <nathanial.fine@gmail.com>
2026-05-28 15:38:52 +0000

    multiBgaManager: match func_001E6C30 (entry init wrapper; cached global + dual reg-pin)

--- src/multiBgaManager.c
+++ src/multiBgaManager.c
@@ -3,6 +3,7 @@
 const char D_00619DC8[24] = "src/multiBgaManager.c";
 
 #include "include_asm.h"
+#include "regpin.h"
 
 INCLUDE_ASM("asm/nonmatchings/src/multiBgaManager", func_001E69D8);
 extern char D_00275850[];
@@ -29,5 +30,19 @@ void func_001E6B00(char *base, int idx, int v6, char *a7, char *a8, int a9)
     *(int *)p = 0;
 }
 INCLUDE_ASM("asm/nonmatchings/src/multiBgaManager", func_001E6B70);
-INCLUDE_ASM("asm/nonmatchings/src/multiBgaManager", func_001E6C30);
+
+extern char D_00276140[];
+
+void func_001E6C30(char *base, int idx, char *a7)
+{
+    char *g = D_00276140;
+    register int neg REG("$3") = -1;
+    register int prod REG("$7") = idx * 0x50;
+    char *p = base + prod;
+    *(int *)(p + 0x40) = neg;
+    func_00105F00(p + 0x10, a7);
+    func_00105F00(p + 0x20, D_00275850);
+    func_0010D830(p + 0x30, (int)g);
+    *(int *)p = 0;
+}
 INCLUDE_ASM("asm/nonmatchings/src/multiBgaManager", func_001E6CA0);
