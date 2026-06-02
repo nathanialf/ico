62a0efd50da9447ecbbdbb7a4703bbf834a542a6
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-02 18:55:04 +0000

    aug6: match ExecFrameDependSequence (TCO -> execEff(a0,0))

--- sugipon/src/frameDependSequence.c
+++ sugipon/src/frameDependSequence.c
@@ -10,7 +10,11 @@ INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/frameDependSequence", playEff);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/frameDependSequence", execEff);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/frameDependSequence", ExecFrameDependSequence);
+extern void execEff(int a0, int a1);
+
+void ExecFrameDependSequence(int a0) {
+    execEff(a0, 0);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/frameDependSequence", executeSEPackageByGObj);
 
