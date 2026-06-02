c7e6116d560907ca57067c6318daf53265cd16e8
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-02 19:07:50 +0000

    aug6: match MakeBoundingBox (clear D_0062BEA0)

--- common/src/PObj.c
+++ common/src/PObj.c
@@ -1,6 +1,10 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/PObj", MakeBoundingBox);
+extern int D_0062BEA0;
+
+void MakeBoundingBox(void) {
+    D_0062BEA0 = 0;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/common/src/PObj", MakePacket);
 
