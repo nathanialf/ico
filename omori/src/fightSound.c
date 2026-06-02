4ba30a817e0e2a0bcd3fb4da07e58cbd02cce91a
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-02 19:41:03 +0000

    aug6: match fightSoundProcess + fightSoundProcessRequestPause (set/return D_0062AA58)

--- omori/src/fightSound.c
+++ omori/src/fightSound.c
@@ -2,8 +2,14 @@
 
 INCLUDE_ASM("asm/aug6/nonmatchings/omori/src/fightSound", fightSoundProcessMain);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/omori/src/fightSound", fightSoundProcess);
+extern int D_0062AA58;
 
-INCLUDE_ASM("asm/aug6/nonmatchings/omori/src/fightSound", fightSoundProcessRequestPause);
+void fightSoundProcess(void) {
+    D_0062AA58 = 0;
+}
+
+int fightSoundProcessRequestPause(void) {
+    return D_0062AA58;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/omori/src/fightSound", fightSoundClose);
