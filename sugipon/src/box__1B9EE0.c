303f689eb7a6a295ba6d2a153aa7d6249ed58508
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-02 19:39:55 +0000

    aug6: match execAutoMove (TCO -> playSEConditionID(a0, 0xD))

--- sugipon/src/box__1B9EE0.c
+++ sugipon/src/box__1B9EE0.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box__1B9EE0", execAutoMove);
+extern void playSEConditionID(int a0, int a1);
+
+void execAutoMove(int a0) {
+    playSEConditionID(a0, 0xD);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box__1B9EE0", initWheels);
