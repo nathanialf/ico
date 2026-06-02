784792f18235b626e3ae021acf6711c7514f9cc7
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-02 19:20:39 +0000

    aug6: match WallLeverDL (TCO -> playSEConditionID(a0, 0x2))

--- sugipon/src/switch__1B9EC8.c
+++ sugipon/src/switch__1B9EC8.c
@@ -1,8 +1,10 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/switch__1B9EC8", WallLeverDL);
-
 extern void playSEConditionID(int a0, int a1);
+
+void WallLeverDL(int a0) {
+    playSEConditionID(a0, 0x2);
+}
 
 void CheckReadyAllSwitches(int a0) {
     playSEConditionID(a0, 0x24);
