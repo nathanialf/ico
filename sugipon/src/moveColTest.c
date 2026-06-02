0badfd41df9fb249f2698c1a3dbcd1771e6a0e21
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-02 19:04:03 +0000

    aug6: match InitMoveColTestGeo (void TCO -> func_0010F048)

--- sugipon/src/moveColTest.c
+++ sugipon/src/moveColTest.c
@@ -4,6 +4,10 @@ INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/moveColTest", MoveColTestGeo);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/moveColTest", MoveColTestDL);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/moveColTest", InitMoveColTestGeo);
+extern void func_0010F048(void *a0);
+
+void InitMoveColTestGeo(void *a0) {
+    func_0010F048(a0);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/moveColTest", func_001E3BE8);
