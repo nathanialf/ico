69a82e8ada91ffa935a8e936402d50aea39132cb
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-03 02:40:40 +0000

    aug6: match renderViewCoordZSphere (sugipon/darkVolume)
    D_0062B5E4 = a1; tail-call MatrixDrive_TurnObjectMatrix(D_004BBC90, a0).

--- sugipon/src/darkVolume.c
+++ sugipon/src/darkVolume.c
@@ -4,7 +4,14 @@ INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/darkVolume", draw);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/darkVolume", drawHT);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/darkVolume", renderViewCoordZSphere);
+extern void MatrixDrive_TurnObjectMatrix(int a0, void *a1);
+extern int D_004BBC90[];
+extern float D_0062B5E4;
+
+void renderViewCoordZSphere(int a0, float a1) {
+    D_0062B5E4 = a1;
+    MatrixDrive_TurnObjectMatrix((int)D_004BBC90, (void *)a0);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/darkVolume", sonic);
 
