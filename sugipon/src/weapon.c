a837496cbba9062cb900f6096685374208afa1e9
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-03 01:59:47 +0000

    aug6: match PickupWeapon (sugipon/weapon)
    ((a0->0x15C)->0x7F0)[0x28] = 0; return 1.

--- sugipon/src/weapon.c
+++ sugipon/src/weapon.c
@@ -46,7 +46,12 @@ INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/weapon", dispInsectNet);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/weapon", WeaponDL);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/weapon", PickupWeapon);
+int PickupWeapon(void *a0) {
+    int *p = *(int **)((char *)a0 + 0x15C);
+    int *q = *(int **)((char *)p + 0x7F0);
+    q[0x28] = 0;
+    return 1;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/weapon", CheckSwapableWeapon);
 
