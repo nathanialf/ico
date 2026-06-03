664fa9ac36ad9256c155a705dcb6c8791c987b57
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-03 22:23:39 +0000

    aug6: match videoCallback
    func_0023C5B0((char*)a2 + 0x50) in jal delay; return 1. rc0, ninja OK.

--- ito/mpeg/mv_videodec.c
+++ ito/mpeg/mv_videodec.c
@@ -12,7 +12,12 @@ INCLUDE_ASM("asm/aug6/nonmatchings/ito/mpeg/mv_videodec", videoDecEndPut);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/ito/mpeg/mv_videodec", videoDecFlush);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/ito/mpeg/mv_videodec", videoCallback);
+extern void func_0023C5B0(void *a0);
+
+int videoCallback(int a0, int a1, void *a2) {
+    func_0023C5B0((char *)a2 + 0x50);
+    return 1;
+}
 
 extern void func_0023C738(void *a0);
 
