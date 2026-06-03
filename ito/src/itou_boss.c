dfe905ec62e65f4bb20978b000f7df5a6ab061db
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-03 02:08:25 +0000

    aug6: match effect_end_func (ito/itou_boss)
    void tail-call func_00260568(D_006CCE50, 0, 0xD50).

--- ito/src/itou_boss.c
+++ ito/src/itou_boss.c
@@ -1,6 +1,11 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/ito/src/itou_boss", effect_end_func);
+extern unsigned char D_006CCE50[];
+extern void func_00260568(void *a0, int a1, int a2);
+
+void effect_end_func(void) {
+    func_00260568(D_006CCE50, 0, 0xD50);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/ito/src/itou_boss", bossCtrlBeforeFunc);
 
