7c426ba81f3101f8683be1ac1fa54c701683bb2c
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-03 01:52:56 +0000

    aug6: match FlagGeo (sugipon/flag)
    (a0->0x4)[0xC] = 1; return 1.

--- sugipon/src/flag.c
+++ sugipon/src/flag.c
@@ -2,7 +2,11 @@
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/flag", InitFlagGeo);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/flag", FlagGeo);
+int FlagGeo(void *a0) {
+    int *t = *(int **)((char *)a0 + 0x4);
+    t[0xC] = 1;
+    return 1;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/flag", FlagDL);
 
