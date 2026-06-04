419243fb56c89affdc8339f7ef3743b7da3a023d
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-04 02:47:58 +0000

    aug6: match ClothTestDL (VU0 out = a + b * s.x macro-mode)

--- sugipon/src/clothTest.c
+++ sugipon/src/clothTest.c
@@ -17,7 +17,18 @@ float InitClothTestGeo(void *a0) {
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/clothTest", ClothTestGeo);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/clothTest", ClothTestDL);
+void ClothTestDL(void *a0, void *a1, void *a2, float a3) {
+    __asm__ __volatile__(
+        ".set noreorder\n"
+        "lqc2 $vf4, 0x0($5)\n"
+        "lqc2 $vf5, 0x0($6)\n"
+        "mfc1 $8, $f12\n"
+        "qmtc2.ni $8, $vf6\n"
+        "vmulx.xyz $vf5, $vf5, $vf6x\n"
+        "vadd.xyz $vf4, $vf4, $vf5\n"
+        "sqc2 $vf4, 0x0($4)\n"
+        ".set reorder\n" : : : "memory");
+}
 
 void func_001C6148(void *a0, void *a1, float a2) {
     __asm__ __volatile__(
