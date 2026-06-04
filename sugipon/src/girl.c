9a833480b9122e00251ebd3609cf2b026e7220b6
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-04 07:19:41 +0000

    aug6: func_001CE7D8 girl — iosPadDevRead(D_00629DEC,a0); return 1

--- sugipon/src/girl.c
+++ sugipon/src/girl.c
@@ -1,6 +1,12 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/girl", func_001CE7D8);
+extern int iosPadDevRead(int a0, void *a1);
+extern int D_00629DEC;
+
+int func_001CE7D8(void *a0) {
+    iosPadDevRead(D_00629DEC, a0);
+    return 1;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/girl", func_001CE800);
 
