f168e5308e050cb08a504364c4150f61f6bd1744
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-04 06:02:36 +0000

    aug6: CheckWallAttributeEdegWall act-env — inflate_stored(a0,a1->0xC,0x5C000); return 1

--- fumi/src/act-env.c
+++ fumi/src/act-env.c
@@ -21,7 +21,12 @@ void GetSofaPosition(void) {
 void GetCollisCenterPositionSimple(void) {
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/act-env", CheckWallAttributeEdegWall);
+extern int inflate_stored(void *a0, int a1, int a2);
+
+int CheckWallAttributeEdegWall(void *a0, int *a1) {
+    inflate_stored(a0, a1[3], 0x5C000);
+    return 1;
+}
 
 void func_001FA070(void) {
 }
