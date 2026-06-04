cf7653fcf93573eaeed23594f0141209388786f3
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-04 23:36:48 +0000

    Match WayPointWithRangeFromPos2 (way_kidnap): doubly-linked-list insert
    Insert node D_004C7CF0[a2] after D_004C7CF0[a1] (stride 64, prev@8/next@0xC);
    return 0.

--- fumi/src/way_kidnap.c
+++ fumi/src/way_kidnap.c
@@ -6,7 +6,19 @@ INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", WayLengthOfPos_Pos);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", WayPointWithRangeFromPos);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", WayPointWithRangeFromPos2);
+typedef struct WayNode { char _0[8]; struct WayNode *prev; struct WayNode *next; char _10[0x30]; } WayNode;
+extern WayNode D_004C7CF0[];
+
+int WayPointWithRangeFromPos2(int a0, int a1, int a2) {
+    WayNode *A = &D_004C7CF0[a1];
+    WayNode *B = &D_004C7CF0[a2];
+    WayNode *old = A->next;
+    A->next = B;
+    B->prev = A;
+    B->next = old;
+    old->prev = B;
+    return 0;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", NearestEnemyFromGirl);
 
