43ffb6658a8e4ecf26a8ca1b4d3769672647b482
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-08 03:18:43 +0000

    Port 7 retail-matched funcs to common/src/haveParentSimpleObj (retail asm-twin port)
    Unified two conflicting func_002604B8 externs to the 3-arg form (asm
    sets $6 at the call sites). ninja SHA green.

--- common/src/haveParentSimpleObj.c
+++ common/src/haveParentSimpleObj.c
@@ -71,17 +71,91 @@ void func_001AB720(void) {
     backStageSave();
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB750);
+extern void debug_assertMessage(char *p, int y);
+extern char D_0060F2F8[];
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB7A8);
+void func_001AB750(int *self, int n, int a2)
+{
+    if (n != 0) {
+        func_002604B8(self[0] + self[1], n);
+    }
+    self[1] += a2;
+    debug_assertMessage(D_0060F2F8, self[1]);
+}
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB7F0);
+extern void MakeGeneratorPacket(int a0);
+extern int GetbufpGeneratorPacket(void);
+extern int GetsizeGeneratorPacket(void);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB858);
+void func_001AB7A8(int a0)
+{
+    MakeGeneratorPacket(a0);
+    {
+        int s0 = GetbufpGeneratorPacket();
+        int s1 = GetsizeGeneratorPacket();
+        return func_001AB750(a0, s0, s1);
+    }
+}
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB8A0);
+extern int GetbufpGeneratorPacket(void);
+extern int GetsizeGeneratorPacket(void);
+extern void func_002604B8(int a0, int a1, int a2);
+extern void ReadGeneratorPacket(void);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB908);
+void func_001AB7F0(int *a0)
+{
+    int s1 = GetbufpGeneratorPacket();
+    int s2 = GetsizeGeneratorPacket();
+    if (s1 != 0) {
+        func_002604B8(s1, a0[0] + a0[1], s2);
+    }
+    a0[1] += s2;
+    return ReadGeneratorPacket();
+}
+
+extern void actBoyHangBefore(int a0);
+extern int actBoyItemAfter(void);
+extern int actBoyCliffHesitate(void);
+
+void func_001AB858(int a0)
+{
+    actBoyHangBefore(a0);
+    {
+        int s0 = actBoyItemAfter();
+        int s1 = actBoyCliffHesitate();
+        return func_001AB750(a0, s0, s1);
+    }
+}
+
+extern int actBoyItemAfter(void);
+extern int actBoyCliffHesitate(void);
+extern void func_002604B8(int a0, int a1, int a2);
+extern void actBoyReadyMove(void);
+
+void func_001AB8A0(int *a0)
+{
+    int s1 = actBoyItemAfter();
+    int s2 = actBoyCliffHesitate();
+    if (s1 != 0) {
+        func_002604B8(s1, a0[0] + a0[1], s2);
+    }
+    a0[1] += s2;
+    return actBoyReadyMove();
+}
+
+void func_001AB908(void)
+{
+    int mask = 0xFFFF;
+    char *p = D_004AF010;
+    int i = 0x8B;
+    p += 0xA80;
+    do {
+        *(short *)(p + 2) = 0;
+        *(short *)(p + 4) = (short)mask;
+        p += 0x40;
+        i--;
+    } while (i >= 0);
+}
 
 void func_001AB948(void) {
     long long *p = (long long *)D_004AF010;
@@ -103,7 +177,15 @@ void func_001AB980(void) {
     }
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB9B8);
+extern int *func_001AB470(int a0, int a3);
+
+int *func_001AB9B8(int a0, int a1, int a2, int a3)
+{
+    int *p = func_001AB470(a0, a3);
+    p[12] = a1;
+    p[13] = a2;
+    return p;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/common/src/haveParentSimpleObj", func_001AB9F8);
 
