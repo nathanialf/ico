8d3572d25a01ed7c23f7904d8d11a914f6faa524
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-08 06:19:04 +0000

    Remove PrintGObjID from tough_nuts (matched)

--- tough_nuts/PrintGObjID/PrintGObjID_seed.c
+++
@@ -1,8 +0,0 @@
-void *PrintGObjID(char *a0, long long a1) {
-    unsigned long long t = (unsigned int)a1;
-    *(int *)(a0 + 8) = 0x53;
-    *(int *)(a0 + 0) = (int)t;
-    *(int *)(a0 + 4) = 0;
-    *(int *)(a0 + 0xC) = 0;
-    return a0 + 0x10;
-}
