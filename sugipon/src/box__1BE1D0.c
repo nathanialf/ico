111b3d5f492f067a2f9c84f043ab7d3863378c0a
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-08 18:15:33 +0000

    Match getAlign (sugipon/box__1BE1D0)

--- sugipon/src/box__1BE1D0.c
+++ sugipon/src/box__1BE1D0.c
@@ -16,7 +16,34 @@ float BoxMemoryFunc(void *a0, void *a1) {
     return MatrixDrive_GetTurnYAngleXZ(func_0023FE70(&buf1, &buf1));
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box__1BE1D0", getAlign);
+int getAlign(float *a0, float f12, float f13, float f14) {
+    float w;
+    float f0;
+    int rv;
+    f13 = f13 + f14;
+    w = a0[2];
+    if (w < 0.0f) {
+        if (-w < f13) goto p4;
+        return 0;
+    }
+    rv = 0;
+    if (!(w < f13)) goto end;
+p4:
+    w = a0[1];
+    if (w < 0.0f) {
+        if (-w < f13) goto rng;
+        return 0;
+    }
+    rv = 0;
+    if (!(w < f13)) goto end;
+rng:
+    f0 = f12 - f13;
+    if (!(f0 + f14 < a0[0])) { rv = 0; goto end; }
+    if (a0[0] < f12 + f13) return 1;
+    rv = 0;
+end:
+    return rv;
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box__1BE1D0", GetDistanceOfGObj);
 
