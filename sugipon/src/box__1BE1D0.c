03aa62cd1f4175ef34bdd29b8385fae46d6b0c41
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-08 17:13:35 +0000

    Match BoxMemoryFunc (sugipon/box__1BE1D0)

--- sugipon/src/box__1BE1D0.c
+++ sugipon/src/box__1BE1D0.c
@@ -1,6 +1,20 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box__1BE1D0", BoxMemoryFunc);
+extern void GetRootMatrixByDObj(void *dst, void *src);
+extern void func_00240008(void *a0, void *a1, void *a2);
+extern float func_0023FE70(void *a0, void *a1);
+extern float MatrixDrive_GetTurnYAngleXZ(float a0);
+
+struct vec4_BE1D0 { float x, y, z, w; } __attribute__((aligned(8)));
+
+float BoxMemoryFunc(void *a0, void *a1) {
+    struct vec4_BE1D0 buf1;
+    struct vec4_BE1D0 buf2;
+    GetRootMatrixByDObj(&buf1, a1);
+    GetRootMatrixByDObj(&buf2, a0);
+    func_00240008(&buf1, &buf1, &buf2);
+    return MatrixDrive_GetTurnYAngleXZ(func_0023FE70(&buf1, &buf1));
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box__1BE1D0", getAlign);
 
