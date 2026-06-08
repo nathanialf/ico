5f3f1c00a6511dc404deb0ceecd5ae4bb523c38a
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-08 05:56:14 +0000

    Port retail-matched funcs to sugiTree + way_kidnap
    sugiTree (dual struct-view of D_00706DA8 via SugiRec + SugiCb __asm__ alias),
    way_kidnap (Nd typedef). ninja SHA green (2b4d7de4).

--- fumi/src/way_kidnap.c
+++ fumi/src/way_kidnap.c
@@ -1,13 +1,28 @@
 #include "common.h"
 
+typedef struct Nd { int pad[2]; struct Nd *f8; struct Nd *fC; char pad2[0x40 - 16]; } Nd;
+
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", add_wp_pos);
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", WayLengthOfPos_Pos);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/way_kidnap", WayPointWithRangeFromPos);
+extern char D_004C6FF0[];
+extern Nd D_004C7CF0[];
+
+int WayPointWithRangeFromPos(int a0, int a1)
+{
+    int *ch = (int *)&D_004C6FF0[a0 * 0x34];
+    Nd *node = &D_004C7CF0[a1];
+    Nd *old;
+    node->f8 = 0;
+    old = (Nd *)ch[2];
+    ch[2] = (int)node;
+    node->fC = old->f8;
+    old->f8 = node;
+    return 0;
+}
 
 typedef struct WayNode { char _0[8]; struct WayNode *prev; struct WayNode *next; char _10[0x30]; } WayNode;
-extern WayNode D_004C7CF0[];
 
 int WayPointWithRangeFromPos2(int a0, int a1, int a2) {
     WayNode *A = &D_004C7CF0[a1];
--- sugipon/src/sugiTree.c
+++ sugipon/src/sugiTree.c
@@ -2,12 +2,30 @@
 
 typedef struct { int w[6]; } SugiRec;
 extern SugiRec D_00706DA8[];
+typedef struct { char _pad[0x10]; int data; void (*fp)(int); } SugiCb;
+extern SugiCb D_00706DA8_cb[] __asm__("D_00706DA8");
 
 void SugiLeafGeo2(int a0, int a1) {
     D_00706DA8[a0].w[5] = a1;
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/sugiTree", SugiLeafDL2);
+extern int D_0062B9E8;
+extern void func_001EDA08(int x);
+
+void SugiLeafDL2(void)
+{
+    int i;
+    if (D_0062B9E8 == 0) {
+        return;
+    }
+    for (i = 0; i < D_0062B9E8; i++) {
+        func_001EDA08(D_00706DA8_cb[i].data);
+        if (D_00706DA8_cb[i].fp != 0) {
+            D_00706DA8_cb[i].fp(D_00706DA8_cb[i].data);
+        }
+    }
+    D_0062B9E8 = 0;
+}
 
 extern void func_001EDA68(void);
 
