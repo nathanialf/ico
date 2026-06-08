15f4c046363aecad55371686a04436165f1b376d
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-08 17:40:29 +0000

    Match landingSE (sugipon/box)

--- sugipon/src/box.c
+++ sugipon/src/box.c
@@ -1,7 +1,30 @@
 #include "common.h"
 #include "ico/types.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/box", landingSE);
+typedef struct {
+    float f_0;
+    float f_4;
+    float f_8;
+    char _C[0x14];
+    float f_20;
+    char _24[0xC];
+    int f_30;
+    char _34[0xC];
+} __attribute__((aligned(8))) SEParams;
+
+extern SEParams D_004B8C40;
+extern void MoveNextStage_Clear(int a0, int a1, int a2, int a3, void *buf, int a5, int a6, int a7);
+
+void landingSE(void *a0, float f0, float f1, float f2, float f3) {
+    SEParams buf = D_004B8C40;
+    buf.f_0 = f0;
+    buf.f_4 = f1;
+    buf.f_8 = f2;
+    buf.f_20 = f3;
+    buf.f_30 = (int)a0;
+    *(int *)a0 = 0;
+    MoveNextStage_Clear(0x3D, 0x44, -1, 0, &buf, -1, 7, 1);
+}
 
 int fallDownStartSE(void *a0) {
     return GOBJ_SUB(a0)->p_7F0->f_8;
