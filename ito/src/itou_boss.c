f7342b1787903b50837381af150cc240b21d1759
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-09 13:42:28 +0000

    Match itou_boss_gflag_init (ito/itou_boss): unsigned do-while keeps up-counter sltiu giv

--- ito/src/itou_boss.c
+++ ito/src/itou_boss.c
@@ -28,7 +28,37 @@ void BossCtrlGeo(void) {
     D_006CCE50[0] = 1;
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/ito/src/itou_boss", itou_boss_gflag_init);
+extern void *isysGObjSearchFromObjLayoutID(int id);
+extern void *isysGObjSearchFromObjKindID_begin(void *o);
+extern int clip_floor_1(void *o);
+extern signed char D_006CCE60[];
+
+int itou_boss_gflag_init(void) {
+    unsigned int s1 = 0;
+    int s2 = 0;
+    void *o;
+    unsigned int i;
+
+    if (isysGObjSearchFromObjLayoutID(0x3F) != 0) {
+        i = 0;
+        do {
+            if (D_006CCE60[i * 0x40 + 4] >= 2) {
+                s1++;
+            }
+            i++;
+        } while (i < 0x35);
+    }
+    o = isysGObjSearchFromObjLayoutID(4);
+    if (o != 0) {
+        do {
+            if (clip_floor_1(o) == 0) {
+                s2++;
+            }
+            o = isysGObjSearchFromObjKindID_begin(o);
+        } while (o != 0);
+    }
+    return s1 >= 0x35 && s2 == 0;
+}
 
 void InqCapsuleGhostBossStage(void) {
 }
