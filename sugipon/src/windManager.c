abbce3b68302a478d3e24bc1528e5794e77f5137
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-10 20:53:34 +0000

    Match ExecWindManager (sugipon/windManager)

--- sugipon/src/windManager.c
+++ sugipon/src/windManager.c
@@ -1,6 +1,15 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/windManager", ExecWindManager);
+extern float D_004C24A0[];
+extern float D_004C2490[];
+extern void func_001D1DF0(void *a0, void *a1, void *a2, void *a3, int a4);
+
+void ExecWindManager(void) {
+    float *p;
+    for (p = D_004C24A0; -1000.0f < *p; p += 8) {
+        func_001D1DF0(p, D_004C2490, p + 4, D_004C2490, -1);
+    }
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/windManager", ReinitWindManager);
 
