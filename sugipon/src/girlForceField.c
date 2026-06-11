7cc3963ecc1d2d06ccb7915d81ce97992ab0b3b8
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-11 02:47:44 +0000

    Match GirlForceFieldDL (sugipon/girlForceField): VU0 dist + parent reattach
    Inline VU0 squared-distance between self and D_00629DE8 root positions;
    in-range path computes a clamped weight and reattaches the parent GObj
    via stage_SetParentOfGObjWithLocalRotationFlag, storing the int result.

--- sugipon/src/girlForceField.c
+++ sugipon/src/girlForceField.c
@@ -19,7 +19,62 @@ void InitGirlForceFieldGeo(void *a0, int a1, int a2) {
     }
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/girlForceField", GirlForceFieldDL);
+typedef struct { float f_0; float f_4; int f_8; } GFField;
+
+typedef struct { char _0[0x34]; int f_34; char _38[0x14]; } NestEnt;   /* 0x4C */
+typedef struct { void *f_0; char _4[0x10]; } RotEnt;                    /* 0x14 */
+
+struct gff_vec { float x, y, z, w; } __attribute__((aligned(16)));
+
+extern void *D_00629DE8;
+extern NestEnt D_002A0A90[];
+extern RotEnt D_0029B128[];
+extern void func_00102828(void *a0);
+extern void func_00102820(void *dst, void *self);
+extern void GetRootMatrixByDObj(void *dst, void *src);
+extern float MatrixDrive_GetTurnYAngleXZ(float a0);
+extern float stage_SetParentOfGObjWithLocalRotationFlag(void *parent, void *vecA,
+                                                        void *vecB, float a3,
+                                                        float a4);
+
+void GirlForceFieldDL(GObj *self) {
+    struct gff_vec self_vec;
+    struct gff_vec buf;
+    struct gff_vec target_vec;
+    GFField *o = (GFField *)GOBJ_SUB(self)->p_7F0;
+    void *tgt;
+
+    func_00102828(self);
+    GetRootMatrixByDObj(&self_vec, self);
+    tgt = D_00629DE8;
+    if (tgt != 0) {
+        float d;
+        GetRootMatrixByDObj(&target_vec, tgt);
+        __asm__ __volatile__(
+            "lqc2 $vf1, 0x0(%1)\n"
+            "lqc2 $vf2, 0x0(%2)\n"
+            "vsub.xyzw $vf3, $vf1, $vf2\n"
+            "vmul.xyz $vf3, $vf3, $vf3\n"
+            "vaddy.x $vf3, $vf3, $vf3y\n"
+            "vaddz.x $vf3, $vf3, $vf3z\n"
+            "qmfc2.ni $2, $vf3\n"
+            "mtc1 $2, %0\n"
+            : "=f"(d) : "r"(&target_vec), "r"(&self_vec) : "$2");
+        if (d < o->f_0 * o->f_0) {
+            float angle = MatrixDrive_GetTurnYAngleXZ(d);
+            float w = 1.0f - angle * o->f_4;
+            w = (w < 0.0f) ? 0.0f : w;
+            func_00102820(&buf, self);
+            o->f_8 = (int)stage_SetParentOfGObjWithLocalRotationFlag(
+                D_0029B128[D_002A0A90[self->f_8].f_34].f_0, &self_vec, &buf,
+                (float)o->f_8, w);
+        } else {
+            o->f_8 = 0;
+        }
+    } else {
+        o->f_8 = 0;
+    }
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/sugipon/src/girlForceField", func_001CF010);
 
