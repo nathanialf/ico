172eae298275718a020ba744127fdfdf8eaf10e4
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-11 21:47:14 +0000

    Match func_00137ED8 (fumi/ios/mcdata)
    3-arg iosMsgSend twin, cmd=0xC.

--- fumi/ios/mcdata.c
+++ fumi/ios/mcdata.c
@@ -55,7 +55,12 @@ int func_00137E90(void *a0, int a1) {
     return iosMsgSend(D_0027D308, a0, 0);
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/ios/mcdata", func_00137ED8);
+int func_00137ED8(void *a0) {
+    McHdr *v = (McHdr *)a0;
+    v->w.hi = 0xC;
+    v->ll = v->ll & -2;
+    return iosMsgSend(D_0027D308, a0, 0);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/ios/mcdata", func_00137F20);
 
