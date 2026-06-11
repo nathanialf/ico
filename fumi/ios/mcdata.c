1852c8628b93501cc68fc7ce4131ad2381cef373
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-11 21:45:26 +0000

    Match func_00137E00 (fumi/ios/mcdata)
    3-arg iosMsgSend twin, cmd=9. Unpark (parked-rc2 4th-arg premise was wrong).

--- fumi/ios/mcdata.c
+++ fumi/ios/mcdata.c
@@ -32,7 +32,12 @@ int func_00137DB8(void *a0) {
     return iosMsgSend(D_0027D308, a0, 0);
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/ios/mcdata", func_00137E00);
+int func_00137E00(void *a0) {
+    McHdr *v = (McHdr *)a0;
+    v->w.hi = 9;
+    v->ll = v->ll & -2;
+    return iosMsgSend(D_0027D308, a0, 0);
+}
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/ios/mcdata", func_00137E48);
 
