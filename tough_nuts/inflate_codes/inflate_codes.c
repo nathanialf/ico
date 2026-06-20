9ac7d8b0a9d597acc3f225ed92b21cbe75d07f37
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-20 11:34:51 +0000

    park inflate_codes (fumi/ios/inflate) — pre-permute seed

--- tough_nuts/inflate_codes/inflate_codes.c
+++ tough_nuts/inflate_codes/inflate_codes.c
@@ -20,7 +20,28 @@ extern int D_0062A258;
 extern int D_0062A268;
 extern char D_00550E48[];
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/ios/inflate", inflate_codes);
+int inflate_codes(InfCodes *a0, int a1, int a2) {
+    int q = a2 / 2048;
+    int new110;
+    do {
+        func_002498E0(a0->f114 + a0->f110 / 2048, q, a1, &D_0062A268);
+        while (func_002490C0(1) != 0) {
+            D_0062A258 = 1;
+            iosSemaCreate();
+            D_0062A258 = 0;
+        }
+        if (func_00249CB0() == 0) {
+            break;
+        }
+        if (((a0->f108 >> 4) & 1) != 0) {
+            break;
+        }
+        debug_assertMessage(&D_00550E48);
+    } while (1);
+    new110 = a0->f110 + a2;
+    a0->f110 = new110;
+    return new110 >= a0->f10C;
+}
 
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/ios/inflate", inflate_stored);
