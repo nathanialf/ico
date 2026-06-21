f5bc3f1ec829dde866ff65efbba7b11a2b4fc9aa
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-21 17:46:35 +0000

    park checkPositionIllegal (fumi/src/act-way) — pre-permute seed

--- tough_nuts/checkPositionIllegal/checkPositionIllegal.c
+++ tough_nuts/checkPositionIllegal/checkPositionIllegal.c
@@ -16,7 +16,31 @@ extern void debug_Marker(void *pos, int r, int g, int b, float size);
 extern Vec3 *ContinueCorrectPosition(void *a0);
 extern float CheckPureWallAttribute(void *a0, int a1);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/act-way", checkPositionIllegal);
+int checkPositionIllegal(void *a0, Vec3 *a1) {
+    float buf1[4];
+    float buf2[4];
+    float f2;
+    if ((*(CheckSub **) ((char *) a0 + 0x164))->f30 != 0x6C) {
+        buf1[0] = a1->f0;
+        buf1[1] = a1->f4;
+        buf1[2] = a1->f8;
+        debug_Marker(buf1, 0, 0xFF, 0, 100.0f);
+        buf2[0] = ContinueCorrectPosition(a0)->f0;
+        buf2[1] = ContinueCorrectPosition(a0)->f4;
+        buf2[2] = ContinueCorrectPosition(a0)->f8;
+        buf2[1] -= CheckPureWallAttribute(a0, 0x2C);
+        debug_Marker(buf2, 0, 0, 0xFF, 100.0f);
+        f2 = buf1[1] - buf2[1];
+        if (f2 < 0.0f) {
+            if (-f2 > 40.0f) {
+                return 1;
+            }
+        } else if (f2 > 40.0f) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 
