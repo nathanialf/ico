1e0e27c529fb6e7b4a145e644320f2e5466d449e
Nathanial Fine <nathanial.fine@gmail.com>
2026-06-30 00:31:44 +0000

    match exit_stage (common/src/StageManager)
    levers: type DE4/DE8 as their real pointer types (void*) storing NULL: changes the TBAA alias set of the 2 gp-stores vs the int struct stores, altering sched1 hoist into the addiu slot
    rc 0 in 55 iters

--- common/src/StageManager.c
+++ common/src/StageManager.c
@@ -1,7 +1,7 @@
 #include "common.h"
 
 extern void func_0013F480(void);
-extern void debug_assertMessage(char *a0);
+extern void debug_assertMessage(char *a0, ...);
 extern void func_0010ED88(void);
 extern void func_00104CF0(void);
 extern void SetDarkVolumeEffect(void);
@@ -50,7 +50,65 @@ void stage_initialize(int a0) {
     _transRingBuf();
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/common/src/StageManager", exit_stage);
+extern void light_getAmbientLight(void *a0, int a1, int a2, int a3);
+extern void func_0023EB60(int a0, int a1);
+extern void func_0019CB28(void);
+extern int func_0013D4B0(int a0);
+extern void ClipStormByCamera(int a0);
+extern void iosThreadWakeup(void *a0, int a1, void *a2, void *a3, int a4, long long a5, int a6);
+extern void iosThreadJoin(void *a0);
+extern void func_0023E168(void);
+extern void func_00240AB8(void);
+extern int func_00240B78(int a0);
+extern void func_001ACA38(void);
+
+extern char D_0060B200[], D_0060B210[];
+extern unsigned char D_00271270[];
+extern int D_00271594_a[] __asm__("D_00271594");
+extern int D_00629CA0;
+extern void *D_00629DE4, *D_00629DE8;
+extern int D_00629E14, D_00629D20;
+extern int D_0062A300, D_0062AAF8, D_0062AAFC, D_0062AB08;
+
+typedef struct { unsigned char _0[0x14]; int f_14; int f_18; } S_00271240;
+extern S_00271240 D_00271240;
+typedef struct { unsigned char _0[0x3c]; unsigned int f_3c; } S_006DE080;
+extern S_006DE080 D_006DE080;
+
+void exit_stage(int a0) {
+    unsigned long long flag;
+    D_00629CA0 = D_00629C90;
+    D_00629C90 = a0;
+    light_getAmbientLight(D_00271270, 1, 1, 1);
+    func_0023EB60(0, 0);
+    D_0062AB08 = 1;
+    func_0019CB28();
+    if (D_0062AAF8 != 0) {
+        func_0013F480();
+        func_0023E168();
+        func_00240AB8();
+        func_00240B78(1);
+        D_0062AAFC = 1;
+        D_00271240.f_14 = 0;
+        D_00271240.f_18 = 0;
+        D_00629DE8 = 0;
+        D_00629DE4 = 0;
+        D_0062AB08 = 0;
+        return;
+    } /* keep goto-equivalent CFG */
+    stop_free_resources();
+    D_0062AB08 = 0;
+    func_0013D4B0(0);
+    ClipStormByCamera(D_00271594_a[0]);
+    D_00629E14 = a0;
+    iosThreadWakeup(&D_006DE080, 1, func_001ACA38, &D_00629C90, D_0062A300, 0x18000, 0x1B);
+    iosThreadJoin(&D_006DE080);
+    flag = D_006DE080.f_3c;
+    debug_assertMessage(D_0060B200, (long long)(int)flag & 1);
+    D_00629D20 = 1;
+    debug_assertMessage(D_0060B210);
+}
+
 
 INCLUDE_ASM("asm/aug6/nonmatchings/common/src/StageManager", start_stage_Load_thread);
 
