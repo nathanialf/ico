2070d806122fa360138a228d84cc16d87f160b13
Nathanial Fine <nathanial.fine@gmail.com>
2026-07-03 00:05:03 +0000

    match func_00167630 (fumi/src/fuzio)
    levers: recover dev C from twin func_00166D00: 3 flag gates (0x70000000 skip, then (F0000!=0x10000 || C0000000==0x80000000) clip), ClipFloor(arg0,e,0,1)
    rc 0 in 2 iters

--- fumi/src/fuzio.c
+++ fumi/src/fuzio.c
@@ -287,7 +287,33 @@ INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/fuzio", func_001673B0);
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/fuzio", func_001674F0);
 
 
-INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/fuzio", func_00167630);
+int func_00167630(void *arg0, int arg1, int arg2) {
+    int ret = 0;
+    int i;
+
+    for (i = 0; i < D_0062C01C; i++) {
+        short *p = D_0062C020->unk18[D_006A4B40[i]];
+        if (p != 0) {
+            while (*p >= 0) {
+                int e = D_0062C020->unk10 + (int) *p * 0x50;
+                int val = *(int *)(e + 0x48);
+                if ((val & 0x70000000) == 0) {
+                    if ((val & 0xF0000) != 0x10000 || (val & 0xC0000000) == 0x80000000) {
+                        if (__ClipFloor(arg0, e, 0, 1) != 0) {
+                            *(int *)((char *)arg0 + 0x88) = e;
+                            ret = 1;
+                            *(int *)((char *)arg0 + 0x80) = arg1;
+                            *(int *)((char *)arg0 + 0x84) = arg2;
+                        }
+                    }
+                }
+                p++;
+            }
+        }
+    }
+    return ret;
+}
+
 
 INCLUDE_ASM("asm/aug6/nonmatchings/fumi/src/fuzio", func_00167798);
 
