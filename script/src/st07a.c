b6367ecc893f0d0fbefc9cc978513f64dce498e1
Nathanial Fine <nathanial.fine@gmail.com>
2026-07-10 13:32:41 +0000

    fan-out W1i: match all 13 st07a.c funcs (BoxBar wrapper family; all first-hypothesis)

--- script/src/st07a.c
+++ script/src/st07a.c
@@ -1,8 +1,57 @@
 #include "common.h"
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aInit);
+typedef struct GObj {
+    char pad[0xB4];
+    int *unkB4;
+} GObj;
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aEnd);
+extern int actInitialize(int a0);
+extern void _ACTWait(int a0);
+extern int func_00178DB0(int a0);
+extern void stage_KillPlayBgAnimation(int a0, int a1, int a2);
+extern void BoxBarSoundOn(int a0, int a1);
+extern int D_004CCE40[];
+extern void actSt08bDoor(volatile int a0);
+
+void actSt07aInit(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF0) == 0) {
+        stage_KillPlayBgAnimation(0x136, 0, 0);
+        D_004CCE40[1] = (int)actSt08bDoor;
+        gobj->unkB4 = D_004CCE40;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        stage_KillPlayBgAnimation(0x136, 0, -1);
+    }
+}
+
+
+extern int D_004CCE60[];
+extern int D_004CCE80[];
+extern void actSt06aDoorDownChk(volatile int a0);
+
+void actSt07aEnd(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0x101) != 0) {
+        stage_KillPlayBgAnimation(0xC8, 0, 0xC8);
+        D_004CCE60[1] = (int)actSt06aDoorDownChk;
+        gobj->unkB4 = D_004CCE60;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        stage_KillPlayBgAnimation(0xC8, 0, 0);
+        D_004CCE80[1] = (int)actSt06aDoorDownChk;
+        gobj->unkB4 = D_004CCE80;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    }
+}
+
 
 extern int actInitialize(int a0);
 extern void _ACTWait(int a0);
@@ -73,21 +122,150 @@ void actSt07aSekizoChk(volatile int a0){
   stage_KillPlayBgAnimation(0x11D, 0, -1);
  } }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aEne);
+extern void gflagInit(int a0);
+extern int D_004CCEE0[];
+extern void actSt08bDoorDownChk(volatile int a0);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aEneChk);
+void actSt07aEne(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF1) == 0) {
+        D_004CCEE0[1] = (int)actSt08bDoorDownChk;
+        gobj->unkB4 = D_004CCEE0;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        gflagInit(0x514);
+    }
+}
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aChan);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aTsuro);
+extern int D_004CCF00[];
+extern void actSt08bKuren(volatile int a0);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aIntro);
+void actSt07aEneChk(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF2) == 0) {
+        D_004CCF00[1] = (int)actSt08bKuren;
+        gobj->unkB4 = D_004CCF00;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        gflagInit(0x515);
+    }
+}
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aSekizo);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aGene1);
+extern int D_004CCF20[];
+extern void actSt08bEne(volatile int a0);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aGene2);
+void actSt07aChan(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF3) != 0 || func_00178DB0(0xF5) != 0) {
+        gflagInit(0x560);
+    } else {
+        D_004CCF20[1] = (int)actSt08bEne;
+        gobj->unkB4 = D_004CCF20;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    }
+}
+
+
+extern int D_004CCF40[];
+extern void actSt08bEnemy1(volatile int a0);
+
+void actSt07aTsuro(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF4) != 0 || func_00178DB0(0xF6) != 0) {
+        gflagInit(0x561);
+    } else {
+        D_004CCF40[1] = (int)actSt08bEnemy1;
+        gobj->unkB4 = D_004CCF40;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    }
+}
+
+
+extern int D_004CCF60[];
+extern void actSt08bEnemy2(volatile int a0);
+
+void actSt07aIntro(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF7) == 0) {
+        D_004CCF60[1] = (int)actSt08bEnemy2;
+        gobj->unkB4 = D_004CCF60;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        gflagInit(0x562);
+    }
+}
+
+
+extern int D_004CCF80[];
+extern void actSt08bKurenMain(volatile int a0);
+
+void actSt07aSekizo(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF8) == 0) {
+        D_004CCF80[1] = (int)actSt08bKurenMain;
+        gobj->unkB4 = D_004CCF80;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        gflagInit(0x563);
+    }
+}
+
+
+extern int D_004CCFA0[];
+extern void actSt08aGirlYoro(volatile int a0);
+
+void actSt07aGene1(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF3) != 0 || func_00178DB0(0xF5) != 0) {
+        gflagInit(0x518);
+    } else {
+        D_004CCFA0[1] = (int)actSt08aGirlYoro;
+        gobj->unkB4 = D_004CCFA0;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    }
+}
+
+
+extern int D_004CCFC0[];
+extern void actSt08bDoorEvent(volatile int a0);
+
+void actSt07aGene2(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF4) != 0 || func_00178DB0(0xF6) != 0) {
+        gflagInit(0x519);
+    } else {
+        D_004CCFC0[1] = (int)actSt08bDoorEvent;
+        gobj->unkB4 = D_004CCFC0;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    }
+}
+
 
 void actSt07aGene3(volatile int a0) {
     int x = a0;
@@ -112,11 +290,62 @@ void actSt07aEne2(volatile int a0) {
     }
 }
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aGene2_1);
+extern int D_004CCD60[];
+extern void func_0021E7E0(volatile int a0);
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aGene2_2);
+void actSt07aGene2_1(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xF9) == 0) {
+        stage_KillPlayBgAnimation(0x12F, 0, 0);
+        D_004CCD60[1] = (int)func_0021E7E0;
+        gobj->unkB4 = D_004CCD60;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        stage_KillPlayBgAnimation(0x12F, 0, -1);
+    }
+}
 
-INCLUDE_ASM("asm/aug6/nonmatchings/script/src/st07a", actSt07aGene2_3);
+
+extern int D_004CCD80[];
+extern void func_0021E8D8(volatile int a0);
+
+void actSt07aGene2_2(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xFA) == 0) {
+        stage_KillPlayBgAnimation(0x130, 0, 0);
+        D_004CCD80[1] = (int)func_0021E8D8;
+        gobj->unkB4 = D_004CCD80;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        stage_KillPlayBgAnimation(0x130, 0, -1);
+    }
+}
+
+
+extern int D_004CCDA0[];
+extern void actSt06aInit(volatile int a0);
+
+void actSt07aGene2_3(volatile int a0) {
+    int x = a0;
+    GObj *gobj = (GObj *)actInitialize(a0);
+    _ACTWait(1);
+    if (func_00178DB0(0xFB) == 0) {
+        stage_KillPlayBgAnimation(0x131, 0, 0);
+        D_004CCDA0[1] = (int)actSt06aInit;
+        gobj->unkB4 = D_004CCDA0;
+        BoxBarSoundOn(a0, 0x189);
+        _ACTWait(0);
+    } else {
+        stage_KillPlayBgAnimation(0x131, 0, -1);
+    }
+}
+
 
 extern int actInitialize(int a0);
 extern void _ACTWait(int a0);
@@ -125,11 +354,6 @@ extern void stage_KillPlayBgAnimation(int a0, int a1, int a2);
 extern void BoxBarSoundOn(int a0, int a1);
 extern int D_004CCDC0[];
 extern void actSt06aSuimon(void);
-
-typedef struct GObj {
-    char pad[0xB4];
-    int *unkB4;
-} GObj;
 
 void actSt07ChanEvent(volatile int a0) {
     int x = a0;
